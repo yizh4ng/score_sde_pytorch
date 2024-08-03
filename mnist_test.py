dc8bb667c7c98da726cc242f08dbcc41aae70c01
ZHANG Yi <yi.zhang@link.cuhk.edu.hk>
2024-08-03 17:41:37 +0800

    Update

--- mnist_test.py
+++ mnist_test.py
@@ -156,85 +156,85 @@ def generate_samples(score_network: torch.nn.Module, nsamples: int) -> torch.Ten
                 x_mean = x_t + step_size * grad / std
                 x_t = x_mean + noise * torch.sqrt(step_size * 2)
 
-    # rtk uld
-    # timesteps = [600, 700, 800, 900, 920, 940, 960, 980, 999]
-    # # timesteps = [100, 200, 300, 400, 500, 600, 700, 800, 830, 860, 900, 920, 940, 960, 980, 999]
-    # # timesteps = [100, 300, 600,  800, 850, 900, 920, 940, 960, 980, 999]
-    # # timesteps = [100, 600, 700, 800, 900, 920, 940, 950, 960, 970, 980, 990, 999]
-    # # timesteps = [100, 300, 600, 700, 800, 900, 920, 940, 960, 980, 999]
-    # timesteps.insert(0, 0)
-    # weight_scale = 1
-    # for index, i in enumerate(timesteps[:-1]):
-    #     current_x = x_t
-    #
-    #     # sde = sde
-    #     t = time_pts[i]
-    #     t_prev = time_pts[timesteps[index + 1]]
-    #     h = t-t_prev
-    #     timestep = (t * (sde.N - 1) / sde.T).int().to(x_t.device)
-    #     timestep_next = ((t-h) * (sde.N - 1) / sde.T).int().to(x_t.device) # same exact thing as  timestep - 1
-    #     alpha_t = sde.alphas_cumprod.to(x_t.device)[timestep]
-    #     alpha_prev = sde.alphas_cumprod.to(x_t.device)[timestep_next]
-    #     t = t.expand(x_t.shape[0], 1)
-    #     predicted_noise = -score_network(x_t, t).detach() * torch.sqrt(1 -alpha_t)
-    #     x0_t = (x_t - (predicted_noise * torch.sqrt((1 - alpha_t)))) / torch.sqrt(alpha_t)
-    #     c1 = 0 * torch.sqrt((1 - alpha_t / alpha_prev) * (1 - alpha_prev) / (1 - alpha_t))
-    #     c2 = torch.sqrt((1 - alpha_prev) - c1 ** 2)
-    #     x_t = torch.sqrt(alpha_prev) * x0_t + c2 * predicted_noise + c1 * torch.randn_like(x_t)
-    #
-    #
-    #     # beta = sde.discrete_betas.to(t.device)[timestep]
-    #     # weight = 4 * beta * h # 0.0011 -> 2e-05
-    #     _beta = 0.5* (beta(1-t_prev) + beta(1-t))
-    #     # _beta = 1
-    #     weight = 2 * (1 - torch.exp(-2 * _beta * h))
-    #     # weight = 4 * h
-    #     std = sde.marginal_prob(x_t, t)[1]
-    #     # ula
-    #     for j in range(100):
-    #             t = time_pts[i]
-    #             timestep = (t * (sde.N - 1) / sde.T).int().to(x_t.device)
-    #             alpha = sde.alphas[timestep]
-    #
-    #             t = t.expand(x_t.shape[0], 1)
-    #             grad = score_network(x_t, t).detach() / std
-    #             term_1 = -(-2 * current_x * torch.exp(-h * _beta)) / weight
-    #             term_2 = -(2 * x_t * torch.exp(-2 * h * _beta)) / weight
-    #             grad = grad + term_1 + term_2
-    #
-    #             noise = torch.randn_like(x_t)
-    #             # step_size = torch.tensor([5e-6]).to(device)
-    #             step_size = (0.1 * std) ** 2 * 2 * alpha * weight
-    #             # step_size = torch.sqrt((0.2 * std) ** 2 * 2 * alpha * weight/200)
-    #             # step_size = weight /  200
-    #             # step_size = torch.tensor(weight/100)
-    #             x_mean = x_t + step_size * grad
-    #             x_t = x_mean + noise * torch.sqrt(step_size * 2)
-    #     # uld
-    #     # v = torch.randn_like(x_t).to(device)
-    #     # for j in range(20):
-    #     #     t = time_pts[i]
-    #     #     timestep = (t * (sde.N - 1) / sde.T).int().to(x_t.device)
-    #     #     alpha = sde.alphas[timestep]
-    #     #
-    #     #     std = sde.marginal_prob(x_t, t)[1]
-    #     #     t = t.expand(x_t.shape[0], 1)
-    #     #     grad = score_network(x_t, t).detach() / std
-    #     #     term_1 = -(-2 * current_x * torch.exp(-h * _beta)) / weight
-    #     #     term_2 = -(2 * x_t * torch.exp(-2 * h * _beta)) / weight
-    #     #     grad = grad + term_1 + term_2
-    #     #
-    #     #     # inner_step_size = torch.tensor([5e-4]).to(device)
-    #     #     inner_step_size = (0.01 * std) ** 2 * 2 * alpha * weight
-    #     #     # inner_step_size = (0.2 * std) ** 2 * 2 * alpha
-    #     #     # inner_step_size = weight / 200
-    #     #     # step_size = torch.tensor(weight/100)
-    #     #
-    #     #     gamma =  1
-    #     #     x_t = x_t + v * inner_step_size
-    #     #     v = (v - gamma * v * inner_step_size
-    #     #          + grad * inner_step_size
-    #     #          + torch.sqrt(torch.tensor(2 * gamma * inner_step_size)) * torch.randn_like(v))
+    rtk uld
+    timesteps = [600, 700, 800, 900, 920, 940, 960, 980, 999]
+    # timesteps = [100, 200, 300, 400, 500, 600, 700, 800, 830, 860, 900, 920, 940, 960, 980, 999]
+    # timesteps = [100, 300, 600,  800, 850, 900, 920, 940, 960, 980, 999]
+    # timesteps = [100, 600, 700, 800, 900, 920, 940, 950, 960, 970, 980, 990, 999]
+    # timesteps = [100, 300, 600, 700, 800, 900, 920, 940, 960, 980, 999]
+    timesteps.insert(0, 0)
+    weight_scale = 1
+    for index, i in enumerate(timesteps[:-1]):
+        current_x = x_t
+
+        # sde = sde
+        t = time_pts[i]
+        t_prev = time_pts[timesteps[index + 1]]
+        h = t-t_prev
+        timestep = (t * (sde.N - 1) / sde.T).int().to(x_t.device)
+        timestep_next = ((t-h) * (sde.N - 1) / sde.T).int().to(x_t.device) # same exact thing as  timestep - 1
+        alpha_t = sde.alphas_cumprod.to(x_t.device)[timestep]
+        alpha_prev = sde.alphas_cumprod.to(x_t.device)[timestep_next]
+        t = t.expand(x_t.shape[0], 1)
+        predicted_noise = -score_network(x_t, t).detach() * torch.sqrt(1 -alpha_t)
+        x0_t = (x_t - (predicted_noise * torch.sqrt((1 - alpha_t)))) / torch.sqrt(alpha_t)
+        c1 = 0 * torch.sqrt((1 - alpha_t / alpha_prev) * (1 - alpha_prev) / (1 - alpha_t))
+        c2 = torch.sqrt((1 - alpha_prev) - c1 ** 2)
+        x_t = torch.sqrt(alpha_prev) * x0_t + c2 * predicted_noise + c1 * torch.randn_like(x_t)
+
+
+        # beta = sde.discrete_betas.to(t.device)[timestep]
+        # weight = 4 * beta * h # 0.0011 -> 2e-05
+        _beta = 0.5* (beta(1-t_prev) + beta(1-t))
+        # _beta = 1
+        weight = 2 * (1 - torch.exp(-2 * _beta * h))
+        # weight = 4 * h
+        std = sde.marginal_prob(x_t, t)[1]
+        # ula
+        for j in range(100):
+                t = time_pts[i]
+                timestep = (t * (sde.N - 1) / sde.T).int().to(x_t.device)
+                alpha = sde.alphas[timestep]
+
+                t = t.expand(x_t.shape[0], 1)
+                grad = score_network(x_t, t).detach() / std
+                term_1 = -(-2 * current_x * torch.exp(-h * _beta)) / weight
+                term_2 = -(2 * x_t * torch.exp(-2 * h * _beta)) / weight
+                grad = grad + term_1 + term_2
+
+                noise = torch.randn_like(x_t)
+                # step_size = torch.tensor([5e-6]).to(device)
+                step_size = (0.1 * std) ** 2 * 2 * alpha * weight
+                # step_size = torch.sqrt((0.2 * std) ** 2 * 2 * alpha * weight/200)
+                # step_size = weight /  200
+                # step_size = torch.tensor(weight/100)
+                x_mean = x_t + step_size * grad
+                x_t = x_mean + noise * torch.sqrt(step_size * 2)
+        # uld
+        # v = torch.randn_like(x_t).to(device)
+        # for j in range(20):
+        #     t = time_pts[i]
+        #     timestep = (t * (sde.N - 1) / sde.T).int().to(x_t.device)
+        #     alpha = sde.alphas[timestep]
+        #
+        #     std = sde.marginal_prob(x_t, t)[1]
+        #     t = t.expand(x_t.shape[0], 1)
+        #     grad = score_network(x_t, t).detach() / std
+        #     term_1 = -(-2 * current_x * torch.exp(-h * _beta)) / weight
+        #     term_2 = -(2 * x_t * torch.exp(-2 * h * _beta)) / weight
+        #     grad = grad + term_1 + term_2
+        #
+        #     # inner_step_size = torch.tensor([5e-4]).to(device)
+        #     inner_step_size = (0.01 * std) ** 2 * 2 * alpha * weight
+        #     # inner_step_size = (0.2 * std) ** 2 * 2 * alpha
+        #     # inner_step_size = weight / 200
+        #     # step_size = torch.tensor(weight/100)
+        #
+        #     gamma =  1
+        #     x_t = x_t + v * inner_step_size
+        #     v = (v - gamma * v * inner_step_size
+        #          + grad * inner_step_size
+        #          + torch.sqrt(torch.tensor(2 * gamma * inner_step_size)) * torch.randn_like(v))
     return x_t
 
 samples = generate_samples(score_network, 20).detach().reshape(-1, 28, 28)
